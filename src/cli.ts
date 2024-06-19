0d797aef1b93e275ea90421c5af81609c134efef
Benjie Gillam <benjie@jemjie.com>
2024-06-19 17:18:05 +0100

    Command-line help and version

--- src/cli.ts
+++ src/cli.ts
@@ -1,39 +1,63 @@
 #!/usr/bin/env node
 import JSON5 from "json5";
-import { parseArgs } from "node:util";
+import { parseArgs, ParseArgsConfig } from "node:util";
 import { checkOperations } from "./main";
 import { open, readdir, stat, readFile, writeFile } from "node:fs/promises";
 import { kjsonlLines } from "kjsonl";
 import { SourceLike } from "./interfaces";
 import { printResults } from "./print.js";
 import { filterBaseline, generateBaseline } from "./baseline";
+import { version } from "./version";
 
-const { values, positionals } = parseArgs({
+const parseArgsConfig = {
   options: {
+    help: {
+      type: "boolean",
+      placeholder: undefined,
+      short: "h",
+      description: "Output available CLI flags",
+    },
+    version: {
+      type: "boolean",
+      placeholder: undefined,
+      short: "v",
+      description: "Output the version",
+    },
     config: {
       type: "string",
+      placeholder: "configPath",
       short: "C",
-      // description: "The path to the config file",
-    },
-    "update-baseline": {
-      type: "boolean",
-      short: "u",
-      // description: "Update the baseline.json file to allow all passed documents even if they break the rules."
+      description: "The path to the config file",
     },
     schema: {
       type: "string",
+      placeholder: "sdlPath",
       short: "s",
-      // description: "Path to the GraphQL schema SDL file"
+      description: "Path to the GraphQL schema SDL file",
     },
     baseline: {
       type: "string",
+      placeholder: "jsonPath",
       short: "b",
-      // description: "Path to the baseline file"
+      description: "Path to the baseline file (.json or .json5)",
+    },
+    "update-baseline": {
+      type: "boolean",
+      placeholder: undefined,
+      short: "u",
+      description:
+        "Update the baseline.json file to allow all passed documents even if they break the rules.",
     },
   },
   allowPositionals: true,
   strict: true,
-});
+} satisfies ParseArgsConfig & {
+  options: {
+    [optionName: string]: { description?: string; placeholder?: string };
+  };
+};
+
+const { values, positionals } = parseArgs(parseArgsConfig);
 
 async function* getOperationsFromKJSONL(
   path: string,
@@ -82,7 +106,43 @@ async function* getOperations(): AsyncIterableIterator<SourceLike> {
   }
 }
 
+function printArg<TKey extends keyof (typeof parseArgsConfig)["options"]>([
+  name,
+  value,
+]: [name: TKey, value: (typeof parseArgsConfig)["options"][TKey]]): string {
+  const { type, short, description, placeholder } = value;
+  return `
+--${name}${type === "boolean" ? "" : ` <${placeholder}>`}\
+${short ? `\n-${short}${type === "boolean" ? "" : ` <${placeholder}>`}` : ""}\
+${
+  description
+    ? `
+
+    ${description?.replace(/\n/g, "\n    ")}`
+    : ""
+}
+`.trim();
+}
+
 async function main() {
+  if (values.help) {
+    console.log(
+      `
+Usage:
+
+  doccheck [-s schema.graphqls] [-b baseline.json5] [-u] doc1.graphql doc2.graphql
+
+Flags:
+
+${(Object.entries(parseArgsConfig.options) as Array<[key: keyof (typeof parseArgsConfig)["options"], value: (typeof parseArgsConfig)["options"][keyof (typeof parseArgsConfig)["options"]]]>).map(printArg).join("\n\n")}
+`.trim(),
+    );
+    return;
+  }
+  if (values.version) {
+    console.log("v" + version);
+    return;
+  }
   const conf: GraphileConfig.Preset["opcheck"] = {
     ...(values.schema ? { schemaSdlPath: values.schema } : null),
     ...(values.baseline ? { baselinePath: values.baseline } : null),
