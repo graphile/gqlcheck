53cb4e86f25809f8a6be16b66dd1a4e2413d92e9
Benjie Gillam <benjie@jemjie.com>
2024-06-19 13:51:30 +0100

    More tests

---
+++ __tests__/depth-limit-large-schema/graphile.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+/** @import {} from '../../dist/index.js' */
+
+const __dirname = new URL(".", import.meta.url).pathname;
+
+/** @type {GraphileConfig.Preset} */
+const preset = {
+  opcheck: {
+    schemaSdlPath: `${__dirname}/schema.graphqls`,
+  },
+};
+
+export default preset;
--- __tests__/depth-limit-large-schema/tests.test.mjs
+++ __tests__/depth-limit-large-schema/tests.test.mjs
@@ -6,7 +6,7 @@ import { getDirHelpers } from "../common.mjs";
 
 const __dirname = new URL(".", import.meta.url).pathname;
 
-it("depth-limit basics", async () => {
+it("depth-limit for large schema", async () => {
   const { getDocuments, configPath, checkResult } =
     await getDirHelpers(__dirname);
   const result = await checkOperations(getDocuments, configPath);
